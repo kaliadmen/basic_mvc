df834b59030bf9da87237f56e4e48d1a7e0c1358
BWorthy <kaliadmen@gmail.com>
2020-05-06 15:47:10 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__FILE__));
+
+//Create array from url
+$url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : [];
+
+require_once(ROOT . DS . 'core' . DS . 'bootstrap.php');
+
