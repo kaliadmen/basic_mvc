d74b954824d472c5b0f9f98b19c88d81e39111a9
BWorthy <kaliadmen@gmail.com>
2020-05-12 17:10:32 -0400

    ADDS BOOTSTRAP AND AUTOLOAD - bootstrap file used to autoload classes - router class started - die and dump helper added -

---
+++ app/lib/helpers/functions.php
@@ -0,0 +1,3 @@
+<?php
+    //list of helpers
+    require_once(ROOT.DS.'app'.DS.'lib'.DS.'helpers'.DS.'helpers.php');
---
+++ app/lib/helpers/helpers.php
@@ -0,0 +1,8 @@
+<?php
+    //Dump and die for debugging
+    function dd($data){
+        echo('<pre>');
+        var_dump($data);
+        echo('</pre>');
+        die();
+    }
