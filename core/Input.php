0866d6c739e03bf0868fa263ba00410c287de1fb
BWorthy <kaliadmen@gmail.com>
2020-09-05 11:31:24 -0500

    ADDS REFACTOR TO INPUT CLASS - refactored get method to use FormHelper class

--- core/Input.php
+++ core/Input.php
@@ -1,15 +1,11 @@
 <?php
     class Input {
 
-        public static function sanitize(string $dirty) : string {
-            return htmlentities($dirty, ENT_QUOTES, "UTF-8");
-        }
-
         public static function get( string $input) : string {
             if(isset($_POST[$input])) {
-                return self::sanitize($_POST[$input]);
+                return FormHelper::sanitize($_POST[$input]);
             } else if(isset($_GET[$input])) {
-                return self::sanitize($_GET[$input]);
+                return FormHelper::sanitize($_GET[$input]);
             }
         }
     }
