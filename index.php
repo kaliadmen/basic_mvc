2a2d99af8029ba20456e01a710597e509b89d81f
BWorthy <kaliadmen@gmail.com>
2020-09-07 00:07:41 -0400

    ADDS REFACTOR TO INDEX - refactored auto load to include validator classes

--- index.php
+++ index.php
@@ -15,6 +15,10 @@ define('ROOT', dirname(__FILE__));
             require_once(ROOT.DS.'app'.DS.'controllers'.DS.$className.'.php');
         }elseif(file_exists(ROOT.DS.'app'.DS.'models'.DS.$className.'.php')){
             require_once(ROOT.DS.'app'.DS.'models'.DS.$className.'.php');
+        }elseif(file_exists(ROOT.DS.'app'.DS.'custom_validators'.DS.$className.'.php')){
+            require_once(ROOT.DS.'app'.DS.'custom_validators'.DS.$className.'.php');
+        }elseif(file_exists(ROOT.DS.'core'.DS.'validators'.DS.$className.'.php')){
+            require_once(ROOT.DS.'core'.DS.'validators'.DS.$className.'.php');
         }
     }
 
