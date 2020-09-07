e67f4742f12650eb73e5c15d660aa3ca78a37121
BWorthy <kaliadmen@gmail.com>
2020-09-07 07:51:33 -0500

    ADDS REFACTOR TO REGISTER CONTROLLER - refactored register action to use validators of Users class

--- app/controllers/RegisterController.php
+++ app/controllers/RegisterController.php
@@ -50,63 +50,54 @@ class RegisterController extends Controller {
     }
 
     public function registerAction() : void {
-        $validation = new Validate();
-        $submitted_values = [
-            'first_name' => '',
-            'last_name' => '',
-            'email' => '',
-            'username' => '',
-            'password' => '',
-            'confirm' => ''
-        ];
+        $new_user = new Users();
 
         if($_SERVER['REQUEST_METHOD'] == "POST") {
-            $submitted_values = FormHelper::post_values($_POST);
-            $validation->validation($submitted_values, [
-                'first_name' => [
-                    'display' => "First name",
-                    'required' => true
-                ],
-                'last_name' => [
-                    'display' => "Last name",
-                    'required' => true,
-                ],
-                'email' => [
-                    'display' => "Email",
-                    'required' => true,
-                    'unique' => 'users',
-                    'max' => 150,
-                    'valid_email' => true
-                ],
-                'username' => [
-                    'display' => "Username",
-                    'required' => true,
-                    'unique' => 'users',
-                    'min' => 5,
-                    'max' => 150
-                ],
-                'password' => [
-                    'display' => "Password",
-                    'required' => true,
-                    'min' => 6
-                ],
-                'confirm' => [
-                    'display' => "Confirm password",
-                    'required' => true,
-                    'matches' => 'password'
-                ],
-            ]);
 
-            if($validation->is_valid()) {
-                $new_user = new Users();
-                $new_user->register_new_user($submitted_values);
-                Router::redirect('register/login');
-            }
+//            $validation->validation($submitted_values, [
+//                'first_name' => [
+//                    'display' => "First name",
+//                    'required' => true
+//                ],
+//                'last_name' => [
+//                    'display' => "Last name",
+//                    'required' => true,
+//                ],
+//                'email' => [
+//                    'display' => "Email",
+//                    'required' => true,
+//                    'unique' => 'users',
+//                    'max' => 150,
+//                    'valid_email' => true
+//                ],
+//                'username' => [
+//                    'display' => "Username",
+//                    'required' => true,
+//                    'unique' => 'users',
+//                    'min' => 5,
+//                    'max' => 150
+//                ],
+//                'password' => [
+//                    'display' => "Password",
+//                    'required' => true,
+//                    'min' => 6
+//                ],
+//                'confirm' => [
+//                    'display' => "Confirm password",
+//                    'required' => true,
+//                    'matches' => 'password'
+//                ],
+//            ]);
+
+                $new_user->assign(FormHelper::post_values($_POST));
+                if($new_user->save()){
+                    Router::redirect('register/login');
+                }
 
         }
 
-        $this->view->display_errors = $validation->display_errors();
-        $this->view->post = $submitted_values;
+        $this->view->display_errors = $new_user->get_error_messages();
+        $this->view->new_user = $new_user;
         $this->view->render('register/register');
     }
 }
