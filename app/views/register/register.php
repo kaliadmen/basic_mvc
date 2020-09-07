cc2feaee4e4279d4da5b7d5801091c35dea1c6cc
BWorthy <kaliadmen@gmail.com>
2020-09-07 07:55:39 -0500

    ADDS REFACTOR TO REGISTER VIEW - refactored form to use FormHelper class methods to build form

--- app/views/register/register.php
+++ app/views/register/register.php
@@ -10,54 +10,25 @@
                     <div class="card">
                         <div class="card-header">Sign up</div>
                         <div class="card-body">
-                            <form name="my-form" onsubmit="return validform()"  action="<?=PROJECTROOT?>register/register" method="post">
-                                <div class="bg-danger"><?=$this->display_errors ?></div>
-                                <div class="form-group row">
-                                    <label for="first_name" class="col-md-4 col-form-label text-md-right">First Name</label>
-                                    <div class="col-md-6">
-                                        <input type="text" name="first_name" id="first_name" class="form-control" placeholder="First Name" value="<?=$this->post['first_name'] ?>">
-                                    </div>
-                                </div>
+                            <form name="my-form" onsubmit="return validform()"  action="" method="post">
+                                <?=FormHelper::display_errors($this->display_errors)?>
 
-                                <div class="form-group row">
-                                    <label for="last_name" class="col-md-4 col-form-label text-md-right">Last Name</label>
-                                    <div class="col-md-6">
-                                        <input type="text" name="last_name" id="last_name" class="form-control" placeholder="Last Name" value="<?=$this->post['last_name'] ?>">
-                                    </div>
-                                </div>
+                                <?=FormHelper::generate_csrf_input()?>
 
-                                <div class="form-group row">
-                                    <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
-                                    <div class="col-md-6">
-                                        <input type="email" name="email" id="email" class="form-control" placeholder="Email" value="<?=$this->post['email'] ?>">
-                                    </div>
-                                </div>
+                                <?=FormHelper::build_input('text', 'first_name', 'First Name', $this->new_user->first_name, ['class' => 'form-group row'], ['class' => 'form-control input-sm']) ?>
 
-                                <div class="form-group row">
-                                    <label for="username" class="col-md-4 col-form-label text-md-right">Username</label>
-                                    <div class="col-md-6">
-                                        <input type="text" name="username" id="username" class="form-control" placeholder="username" value="<?=$this->post['username'] ?>">
-                                    </div>
-                                </div>
+                                <?=FormHelper::build_input('text', 'last_name', 'Last Name', $this->new_user->last_name, ['class' => 'form-group row'], ['class' => 'form-control input-sm']) ?>
 
-                                <div class="form-group row">
-                                    <label for="passowrd" class="col-md-4 col-form-label text-md-right">Password</label>
-                                    <div class="col-md-6">
-                                        <input type="password" name="password" id="password" class="form-control" placeholder="password">
-                                    </div>
-                                </div>
+                                <?=FormHelper::build_input('text', 'email', 'Email', $this->new_user->email, ['class' => 'form-group row'], ['class' => 'form-control input-sm']) ?>
 
-                                <div class="form-group row">
-                                    <label for="confirm" class="col-md-4 col-form-label text-md-right">Confirm Password</label>
-                                    <div class="col-md-6">
-                                        <input type="password" name="confirm" id="confirm" class="form-control" placeholder="confirm password">
-                                    </div>
-                                </div>
+                                <?=FormHelper::build_input('text', 'username', 'Username', $this->new_user->username, ['class' => 'form-group row'], ['class' => 'form-control input-sm']) ?>
+
+                                <?=FormHelper::build_input('password', 'password', 'Password', $this->new_user->password, ['class' => 'form-group row'], ['class' => 'form-control input-sm']) ?>
+
+                                <?=FormHelper::build_input('password', 'confirm', 'Confirm Password', '', ['class' => 'form-group row'], ['class' => 'form-control input-sm']) ?>
 
                                 <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary mr-3">
-                                        Register
-                                    </button>
+                                    <?=FormHelper::build_submit('Register', ['class' => 'btn btn-primary mr-3']) ?>
 
                                     Already have an account?<a href="<?=PROJECTROOT?>register/login">Sign In</a>
                                 </div>
