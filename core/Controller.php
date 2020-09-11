0be80881221ce44da4cc736518e0602e0dab74a7
BWorthy <kaliadmen@gmail.com>
2020-09-11 15:48:06 -0400

    ADDS REFACTOR TO CONTROLLER CLASS - added request property - refactored constructor method

--- core/Controller.php
+++ core/Controller.php
@@ -3,16 +3,17 @@
     class Controller extends Application {
 
         protected $_controller, $_action;
-        public $view;
+        public $view, $request;
 
         public function __construct($controller, $action) {
             parent::__construct();
             $this->_controller = $controller;
             $this->_action = $action;
+            $this->request = new Input();
             $this->view = new View();
         }
 
-        protected function load_model(string $model) {
+        protected function load_model(string $model) : void {
             if(class_exists($model)) {
                 $this->{$model.'_Model'} = new $model(strtolower($model));
             }
