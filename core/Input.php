8b49568bced4c82d614522769a08a8f74f5776a0
BWorthy <kaliadmen@gmail.com>
2020-09-11 15:40:00 -0400

    ADDS REFACTOR TO INPUT CLASS - added methods to check for server request method - refactored get method to return array - added csrf token validation method

--- core/Input.php
+++ core/Input.php
@@ -1,11 +1,44 @@
 <?php
     class Input {
 
-        public static function get( string $input) : string {
-            if(isset($_POST[$input])) {
-                return FormHelper::sanitize($_POST[$input]);
-            } else if(isset($_GET[$input])) {
-                return FormHelper::sanitize($_GET[$input]);
+        public function is_post() : bool {
+            return $this->get_request_method() === 'POST';
+        }
+
+        public function is_get() : bool {
+            return $this->get_request_method() === 'GET';
+        }
+
+        public function is_put() : bool {
+            return $this->get_request_method() === 'PUT';
+        }
+
+        public function is_delete() : bool {
+            return $this->get_request_method() === 'DELETE';
+        }
+
+        public function get_request_method() : string {
+            return strtoupper($_SERVER['REQUEST_METHOD']);
+        }
+
+        public function get( string $input = '') : array {
+            if(!(bool)$input) {
+                // return sanitized request array
+                $data = [];
+                foreach($_REQUEST as $column => $value) {
+                    $data[$column] = FormHelper::sanitize($value);
+                }
+                return $data;
             }
+
+            return (array)FormHelper::sanitize($_REQUEST[$input]);
+        }
+
+        public function has_valid_csrf_token() : bool {
+            if(!FormHelper::validate_token($this->get('csrf_token')[0])) {
+                Router::redirect('restricted/csrf');
+                return false;
+            }
+            return true;
         }
     }
