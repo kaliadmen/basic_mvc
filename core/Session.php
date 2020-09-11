be6e846032708d47571c070d943c43fd572225f9
BWorthy <kaliadmen@gmail.com>
2020-09-11 17:26:57 -0500

    ADDS SESSION MESSAGES - added method to set messages on the session - added method to display message as bootstrap alert

--- core/Session.php
+++ core/Session.php
@@ -25,4 +25,33 @@
          $uagent_string = preg_replace($regx, '', $uagent);
          return $uagent_string;
      }
+
+     public static function display_message() : string {
+         $alert_types = ['alert-info', 'alert-success', 'alert-warning', 'alert-danger'];
+
+         $html = '';
+
+         foreach($alert_types as $alert_type) {
+             if(self::exists($alert_type)) {
+                 $html .= '<div class="alert alert-dismissible '.$alert_type.'" role="alert">';
+                 $html .= '<button type="button" class="close" data-dismiss="alert" aria-label="Close">';
+                 $html .= '<span aria-hidden="true">&times;</span></button>';
+                 $html .= self::get($alert_type);
+                 $html .= '</div>';
+                 self::delete($alert_type);
+             }
+         }
+
+         return $html;
+     }
+
+     /**
+      * Sets session alert message
+      * @param string $type expects info, success, warning, or danger
+      * @param string $message message to display in alert
+      */
+     public static function set_message(string $type, string $message) : void {
+        $session_name = 'alert-'.$type;
+        self::set($session_name, $message);
+     }
  }
